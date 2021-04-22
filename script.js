1327016e51da77ef324e17d68ef314aebdbede1a
mkindlinger <78495229+mkindlinger@users.noreply.github.com>
2021-04-22 11:49:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 pic1 = document.getElementById('pic1');
 pic2 = document.getElementById('pic2');
-pseudoCanvas = document.getElementById('pic2');
+pseudoCanvas = document.getElementById('pseudocanvas');
 
 pic1.addEventListener('click', (e) => {
     pseudoCanvas.src = '1051.jpg';
